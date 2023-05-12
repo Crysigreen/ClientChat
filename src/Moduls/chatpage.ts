fa45af34a53e8d2228649ab0ea8a6532d6ddee6d
Crysigreen <ilya.korzhov92@gmail.com>
2023-05-13 00:06:41 +0300

    Added plain text display

--- src/Moduls/chatpage.ts
+++ src/Moduls/chatpage.ts
@@ -15,13 +15,17 @@ type Message = {
       <div class="bg-white border-b border-gray-300 h-16 px-5 py-5 flex justify-between items-center">
         <div class="font-bold text-gray-600">{{ chatPartner }}</div>
         <div>
-          <button class="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-full mr-2"><img src="../assets/CallIcon.svg" alt="video call" class="w-6 h-6"></button>
-          <button class="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-full"><img src="../assets/CallIcon.svg" alt="call" class="w-6 h-6"></button>
+          <button class="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-full mr-2">
+            <img src="../assets/CallIcon.svg" alt="video call" class="w-6 h-6">
+          </button>
+          <button class="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-full">
+            <img src="../assets/CallIcon.svg" alt="call" class="w-6 h-6">
+          </button>
         </div>
       </div>
-      <div class="flex-1 overflow-y-scroll px-4 py-2 content-end">
-        <div *ngFor="let message of messages">
-          <div [ngClass]="{ 'ml-auto': message.from === 'me' }" class="bg-white rounded-lg p-2 mb-2 w-3/4">
+      <div class="flex-1 overflow-y-auto px-4 py-2">
+        <div *ngFor="let message of messages" class="flex justify-end">
+          <div [ngClass]="{'ml-auto': message.from === 'me'}" class="bg-white rounded-lg p-2 mb-2 inline-block max-w-xs whitespace-normal break-words">
             <div class="text-gray-600">{{ message.content }}</div>
             <div class="text-xs text-gray-400">{{ message.timestamp | date:'shortTime' }}</div>
           </div>
